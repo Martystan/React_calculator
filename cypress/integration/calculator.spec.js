b68a6d475f5f8dab519ca53d74b8eada99166fae
Martyna Stanislawska <martynastan@gmail.com>
2022-01-04 20:55:55 +0100

    integration tests passing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,62 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display of the running total', ()=>{
+    cy.get('#number2').click();
+    cy.get("#operator_add").click();
+    cy.get('#number1').click();
+    cy.get("#operator-equals").click();
+    cy.get("#running-total").should("contain", "3")
+  })
+
+  it('should update the display with the result of the operation', ()=>{
+    cy.get('#number2').click();
+    cy.get("#operator_add").click();
+    cy.get('#number1').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '3')
+
+  })
+
+  it('should chain multiple operations', ()=>{
+    cy.get('#number2').click();
+    cy.get("#operator_add").click();
+    cy.get('#number1').click();
+    cy.get("#operator-subtract").click();
+    cy.get('#number1').click();
+    cy.get("#operator-multiply").click();
+    cy.get('#number2').click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '4')
+    
+  })
+
+  it('should output range of numbers', ()=>{
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number5").click();
+    cy.get("#operator_add").click();
+    cy.get('#number1').click();
+    cy.get('#number1').click();
+    cy.get('#number1').click();
+    cy.get("#operator-multiply").click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get("#number0").click();
+    cy.get("#decimal").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get('.display').should('contain', '2422012.6999999997')
+  })
+
+  it('should return undefined when dividing by 0', () => {
+    cy.get("#number6").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number0").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should('contain', 'undefined')
+  })
 })
